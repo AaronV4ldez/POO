6286d6dd77beee487a71b543a8e6a7f3ac0d5f5a
AaronV4ldez <73905734+AaronV4ldez@users.noreply.github.com>
2021-03-09 15:49:05 -0700

    Hola mundo
    El pograma de hola mundo parte 2

---
+++ Hola mundo/src/Principal.java
@@ -0,0 +1,14 @@
+public class Principal {
+    public static void main(String[] args) { //psvm
+short x1=1;
+int x=6;
+short x3=12;
+boolean entro= false;
+String texto =" Hello darknes my old friend UwU ";
+
+int numero_1= 11;
+int numero_2= 24;
+        System.out.println(--numero_1 + numero_2++);
+        System.out.println(numero_2 + texto + --numero_1);
+    }
+}
