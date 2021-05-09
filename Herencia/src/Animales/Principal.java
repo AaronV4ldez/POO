c3893893f6d65586f2a1c267e1f0b27a07daf2d3
AaronV4ldez <73905734+AaronV4ldez@users.noreply.github.com>
2021-05-09 03:22:17 -0600

    clase arreglo
    uwu

--- Herencia/src/Animales/Principal.java
+++ Herencia/src/Animales/Principal.java
@@ -1,9 +1,27 @@
 package Animales;
 
 import java.awt.*;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
+        int[] miArray = new int[4];
+        String [] str;
+        Scanner scanner=new Scanner((System.in);
+        System.out.println("Ciamtps registros kieres¿");
+        int c = scanner.nextInt();
+        str =new String[c];
+        for (int x=0; x<c; x++){
+            System.out.println("Inserta Nombre");
+            String n = scanner.next();
+            str[x]=n;
+        }
+        imprimirArreglo(str);
+
+        Ave[] arrayAves= new Ave[5];
+        Animal[] arrayAnimal=new Animal[5];
+        Perro [] arrayPerro= new Perro[5];
+
 Animal animalGenerico1xd1linkmega =new Animal(Color.cyan, 24);
 Ave zapdos = new Ave(Color.black, 45, "electrico");
 Perro popi = new Perro(Color.black, 30, "Chihuahua");
@@ -11,5 +29,11 @@ animalGenerico1xd1linkmega.comer();
 zapdos.volar();
 
 
+
+    }
+    public static void imprimirArreglo (String[] arr){
+        for (int x=0; x<arr.length; x++){
+            System.out.println(arr[x]);
+        }
     }
 }
